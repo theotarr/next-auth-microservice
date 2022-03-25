ab7d48fbe482f8b65d8da654732ebe7bd85d7139
Theo <theodore@tarr.com>
2022-03-25 16:49:14 -0400

    remove jest

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import useSWR from 'swr'
-import HelloWorld from '@src/components/HelloWorld'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { PrismaClient, User } from '@prisma/client'
 
 const Home = ({ users }: { users: User[] }) => {
   const { data: session } = useSWR(
     `${process.env.NEXTAUTH_URL}/api/auth/session`,
   )
+  const router = useRouter()
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -16,21 +17,43 @@ const Home = ({ users }: { users: User[] }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <HelloWorld />
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.email}</li>
-          ))}
-        </ul>
-        <div className="bg-yellow-200 h-12 w-12 text-black">
-          <h1>{session?.user?.email}</h1>
+      <main className="text-center text-black ">
+        <div className="bg-yellow-200 rounded-md">
+          <h1 className="font-bold">
+            {session?.user?.name || 'not signed in'}
+          </h1>
           <br />
-          <a
-            href={`${process.env.NEXTAUTH_URL}/api/auth/signin?redirectCallback=http://localhost:3000`}
-          >
-            signIn
-          </a>
+          {session?.user ? (
+            <a
+              href={`${
+                process.env.NEXTAUTH_URL
+              }/api/auth/signout?redirectCallback=${
+                (process.env.VERCEL_URL || `http://localhost:3000`) +
+                router.asPath
+              }`}
+            >
+              Sign out
+            </a>
+          ) : (
+            <a
+              href={`${
+                process.env.NEXTAUTH_URL
+              }/api/auth/signin?redirectCallback=${
+                (process.env.VERCEL_URL || `http://localhost:3000`) +
+                router.asPath
+              }`}
+            >
+              Sign in
+            </a>
+          )}
+        </div>
+        <div className="bg-gray-200 w-50 h-20 mt-4 p-4 rounded-md">
+          <h1 className="h1 font-bold">List Of Users ({users.length})</h1>
+          {users.map(user => (
+            <div key={user.id}>
+              {user.name} - {user.email}
+            </div>
+          ))}
         </div>
       </main>
     </div>
